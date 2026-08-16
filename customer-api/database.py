4e104dbf5b89299f6b3044d5833131bf3d4a553c
Saban-Adhikari <101613059+Saban-Adhikari@users.noreply.github.com>
2026-08-16 22:23:28 +0545

    Remove obsolete SQLite initialization code

--- customer-api/database.py
+++ customer-api/database.py
@@ -1,23 +1,3 @@
-# import sqlite3
-
-# connection = sqlite3.connect("customers.db")
-
-# cursor = connection.cursor()
-
-# cursor.execute("""
-#     CREATE TABLE IF NOT EXISTS customers (
-#          id INTEGER PRIMARY KEY AUTOINCREMENT,
-#          name TEXT NOT NULL
-#          )
-#     """)
-
-# cursor.execute("INSERT INTO customers (name) VALUES (?)", ("John Smith",))
-# cursor.execute("INSERT INTO customers (name) VALUES (?)", ("Sarah Jones",))
-# cursor.execute("INSERT INTO customers (name) VALUES (?)", ("Bob Williams",))
-
-# connection.commit()
-# connection.close()
-
 import sqlite3
 
 
